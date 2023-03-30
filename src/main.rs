e553f32e0f5a0bcd23abbe5e968947d703971b79
Andrei Panov <andrei.panov@sap.com>
2023-03-30 19:57:49 +0200

    проблемы с лайфтаймом

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,38 @@
-use std::process::Output;
 
 fn main() {
     let start_medved = start_with("medved");
     let start_preved = start_with("preved");
 
-    let and = vec![start_medved, start_preved];
+    let both: Vec<ParserFunc> = vec![start_medved, start_preved];
 
-    for func in and {
-        println!("{:?}", func("medved preved"))
-    }
+    let anyParser = any(both);
+    let res = anyParser("preved medved");
 
-
-    let f1: ParserFunc = Box::new(|input:&str| Ok(("res1", "res2")));
-
-    let x = f1.parse("privet");
-    println!("{:?}", x)
-}
-
-impl Parser for Box<dyn Fn(&str) -> IResult<&str, &str>> {
-    type Input = &'static str;
-    type Output = &'static str;
-
-    fn parse(&self, input: &str) -> IResult<Self::Input, Self::Output> {
-        println!("вызов метода Парс у парсера!");
-        Ok(("","из парсера"))
-    }
-}
-
-trait Parser {
-    type Input;
-    type Output;
-
-    fn parse(&self, input: &str) -> IResult<Self::Input,Self::Output>;
-}
-
-
-type ParserFunc = Box<dyn Fn(&str) -> IResult<&str, &str>>;
-
-fn start_with<'t, 'i>(with: &'t str) -> impl Fn(&'i str) -> IResult<&'i str, &'i str> + 't {
-    return move |input: &str| {
-        return if input.starts_with(&with) {
-            Ok((&input[with.len()..], &input[..with.len()]))
-        } else {
-            Err("не шмогла".to_string())
-        };
-    };
+    println!("{:?}", &res);
 }
 
 type IResult<IN, OUT> = Result<(IN, OUT), String>;
+type ParserFunc<'i> = Box<dyn Fn(&'i str) -> IResult<&'i str, &'i str>>;
+
+fn any<'i>(functions: Vec<ParserFunc<'i>>) -> ParserFunc<'i> {
+    Box::new(move |input: &'i str| {
+        for function in &functions {
+            let res: IResult<&str, &str> = function(input);
+            if res.is_ok() {
+                return res;
+            }
+        }
+        return Err("все плохо!".to_string());
+    })
+}
+
+fn start_with<'t, 'i>(with: &'t str) -> ParserFunc<'i> {
+    Box::new(move |input: &'i str| {
+        return if input.starts_with(&with) {
+            Ok((&input[with.len()..], &input[..with.len()]))
+        } else {
+            Err("".to_string())
+        };
+    })
+}
+
